81ec98ed80eb77ad73261312947e6b8310a51316
xuanwushijie <1091237632@qq.com>
2018-06-22 09:30:50 +0800

    “demo”

---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import logo from './imgs/logo.png'
+import './logo.less'
+/*
+简单的显示logo的组件
+ */
+export default class Logo extends Component {
+  render () {
+    return (
+      <div className="logo-container">
+        <img src={logo} alt="logo" className='logo-img'/>
+      </div>
+    )
+  }
+}
